97b82e18d35052ec47f2c97a02a99ef9c01bd9cc
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-01-08 22:19:34 +0530

    Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/components/AdminSidebar.tsx
    	modified:   src/styles/_mediaquery.scss
    	modified:   src/styles/app.scss

--- src/components/AdminSidebar.tsx
+++ src/components/AdminSidebar.tsx
@@ -1,50 +1,106 @@
-import { Link, Location, useLocation } from "react-router-dom";
-import { RiCoupon3Fill, RiDashboardFill, RiShoppingBag3Fill } from "react-icons/ri";
-import { IoIosPeople } from "react-icons/io";
-import { AiFillFileText } from "react-icons/ai";
-import { FaChartBar, FaChartPie, FaChartLine, FaStopwatch, FaGamepad } from "react-icons/fa";
+import { useEffect, useState } from "react";
 import { IconType } from "react-icons";
+import { AiFillFileText } from "react-icons/ai";
+import {
+  FaChartBar,
+  FaChartLine,
+  FaChartPie,
+  FaGamepad,
+  FaStopwatch,
+} from "react-icons/fa";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { IoIosPeople } from "react-icons/io";
+import {
+  RiCoupon3Fill,
+  RiDashboardFill,
+  RiShoppingBag3Fill,
+} from "react-icons/ri";
+import { Link, Location, useLocation } from "react-router-dom";
 
 const AdminSidebar = () => {
   const location = useLocation();
 
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [phoneActive, setPhoneActive] = useState<boolean>(
+    window.innerWidth < 1100
+  );
+
+  const resizeHandler = () => {
+    setPhoneActive(window.innerWidth < 1100);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", resizeHandler);
+
+    return () => {
+      window.removeEventListener("resize", resizeHandler);
+    };
+  }, []);
+
   return (
-    <aside>
-      <h2>Logo.</h2>
-      <DivOne location={location} />
-      <DivTwo location={location} />
-      <DivThree location={location} />
-    </aside>
+    <>
+      {phoneActive && (
+        <button id="hamburger" onClick={() => setShowModal(true)}>
+          <HiMenuAlt4 />
+        </button>
+      )}
+
+      <aside
+        style={
+          phoneActive
+            ? {
+                width: "20rem",
+                height: "100vh",
+                position: "fixed",
+                top: 0,
+                left: showModal ? "0" : "-20rem",
+                transition: "all 0.5s",
+              }
+            : {}
+        }
+      >
+        <h2>Logo.</h2>
+        <DivOne location={location} />
+        <DivTwo location={location} />
+        <DivThree location={location} />
+
+        {phoneActive && (
+          <button id="close-sidebar" onClick={() => setShowModal(false)}>
+            Close
+          </button>
+        )}
+      </aside>
+    </>
   );
 };
 
 const DivOne = ({ location }: { location: Location }) => (
   <div>
-    <h5> DASHBOARD</h5>
+    <h5>Dashboard</h5>
     <ul>
       <Li
-        url={"/admin/dashboard"}
-        text={"Dashboard"}
-        location={location}
+        url="/admin/dashboard"
+        text="Dashboard"
         Icon={RiDashboardFill}
+        location={location}
       />
       <Li
-        url={"/admin/product"}
-        text={"product"}
-        location={location}
+        url="/admin/product"
+        text="Product"
         Icon={RiShoppingBag3Fill}
+        location={location}
       />
       <Li
-        url={"/admin/customer"}
-        text={"Customer"}
-        location={location}
+        url="/admin/customer"
+        text="Customer"
         Icon={IoIosPeople}
+        location={location}
       />
       <Li
-        url={"/admin/transaction"}
-        text={"Transaction"}
-        location={location}
+        url="/admin/transaction"
+        text="Transaction"
         Icon={AiFillFileText}
+        location={location}
       />
     </ul>
   </div>
@@ -52,25 +108,25 @@ const DivOne = ({ location }: { location: Location }) => (
 
 const DivTwo = ({ location }: { location: Location }) => (
   <div>
-    <h5> Charts </h5>
+    <h5>Charts</h5>
     <ul>
       <Li
-        url={"/admin/chart/bar"}
-        text={"Bar"}
-        location={location}
+        url="/admin/chart/bar"
+        text="Bar"
         Icon={FaChartBar}
+        location={location}
       />
       <Li
-        url={"/admin/chart/pie"}
-        text={"Pie"}
-        location={location}
+        url="/admin/chart/pie"
+        text="Pie"
         Icon={FaChartPie}
+        location={location}
       />
       <Li
-        url={"/admin/chart/line"}
-        text={"Line"}
-        location={location}
+        url="/admin/chart/line"
+        text="Line"
         Icon={FaChartLine}
+        location={location}
       />
     </ul>
   </div>
@@ -78,25 +134,25 @@ const DivTwo = ({ location }: { location: Location }) => (
 
 const DivThree = ({ location }: { location: Location }) => (
   <div>
-    <h5> Apps</h5>
+    <h5>Apps</h5>
     <ul>
       <Li
-        url={"/admin/app/stopwatch"}
-        text={"Stopwatch"}
-        location={location}
+        url="/admin/app/stopwatch"
+        text="Stopwatch"
         Icon={FaStopwatch}
+        location={location}
       />
       <Li
-        url={"/admin/app/coupon"}
-        text={"Coupon"}
-        location={location}
+        url="/admin/app/coupon"
+        text="Coupon"
         Icon={RiCoupon3Fill}
+        location={location}
       />
       <Li
-        url={"/admin/app/toss"}
-        text={"Toss"}
-        location={location}
+        url="/admin/app/toss"
+        text="Toss"
         Icon={FaGamepad}
+        location={location}
       />
     </ul>
   </div>
@@ -108,7 +164,6 @@ interface LiProps {
   location: Location;
   Icon: IconType;
 }
-
 const Li = ({ url, text, location, Icon }: LiProps) => (
   <li
     style={{
@@ -120,7 +175,7 @@ const Li = ({ url, text, location, Icon }: LiProps) => (
     <Link
       to={url}
       style={{
-        color: location.pathname.includes(url) ? "rgba(0,115,255)" : "black",
+        color: location.pathname.includes(url) ? "rgb(0,115,255)" : "black",
       }}
     >
       <Icon />
